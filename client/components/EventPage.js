c8ee1bb37b3c792104b142148ed9970ff5500966
Chris Choi <chc5139@gmail.com>
2019-08-12 10:31:08 -0400

    search

--- client/components/EventPage.js
+++ client/components/EventPage.js
@@ -7,27 +7,62 @@ class EventPage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      concerts: []
+      concerts: [],
+      search: '',
+      concerts1: []
     }
   }
   async componentDidMount() {
     try {
       const concerts = await axios.get(`/api/concerts`)
       if (concerts) {
-        this.setState({concerts: concerts.data})
+        this.setState({concerts: concerts.data, concerts1: concerts.data})
       }
     } catch (error) {
       console.error(error)
     }
   }
+  handleChange = event => {
+    this.setState({search: event.target.value})
+
+    let concerts = this.state.concerts.filter(concert => {
+      let str = ''
+      let keys = Object.keys(concert)
+      console.log(keys)
+      for (let i = 1; i < 5; i++) {
+        str += concert[keys[i]]
+      }
+      console.log(str)
+      let regex = new RegExp(this.state.search, 'gi')
+      console.log(regex)
+      return str.match(regex)
+    })
+    console.log(concerts)
+    this.setState({
+      concerts1: concerts
+    })
+  }
 
   render() {
-    const concerts = this.state.concerts
+    const concerts = this.state.concerts1
     return (
       <div className="wrapper tutorial-page">
         <div className="main main-raised main-translucent">
           <div className="section">
             <div className="container">
+              <div className="input-group mt-5">
+                <div className="input-group-prepend bg-dark b-dark">
+                  <div className="input-group-text bg-dark text-white">@</div>
+                </div>
+                <input
+                  type="text"
+                  value={this.state.search}
+                  onChange={this.handleChange}
+                  className="form-control"
+                  id="inlineFormInputGroup"
+                  placeholder="Search"
+                />
+              </div>
               <table className="table table-hover table-responsive mt-5">
                 <thead className="thead-dark">
                   <tr className="text-white">
