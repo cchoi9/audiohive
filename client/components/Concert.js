94213d9a3e6cefbb5b368104c379422d84ccef47
Chris Choi <chc5139@gmail.com>
2019-08-11 22:11:10 -0400

    draft completed

--- client/components/Concert.js
+++ client/components/Concert.js
@@ -3,22 +3,16 @@ import React from 'react'
 export const Concert = props => {
   const concert = props.concert
   return (
-    <div className="concert">
-      <div className="photo">
-        {/* <main>
-          <img className="school-pic" src={concert.imageUrl} />
-        </main> */}
-      </div>
-      <div className="details">
-        <main>
-          <h3>{concert.artist}</h3>
-          <h4>{concert.venue}</h4>
-          <p>
-            {concert.date} {concert.time}
-          </p>
-          <a href={concert.tickets}>Tickets</a>
-        </main>
-      </div>
-    </div>
+    <tr className="text-white" key={concert.id}>
+      <td className="text-center">{concert.date}</td>
+      <td>{concert.time}</td>
+      <td>{concert.artist}</td>
+      <td>{concert.venue}</td>
+      <td className="td-tickets text-right">
+        <a href={concert.tickets} className="btn btn-primary">
+          Tickets
+        </a>
+      </td>
+    </tr>
   )
 }
--- client/components/EventPage.js
+++ client/components/EventPage.js
@@ -24,18 +24,35 @@ class EventPage extends React.Component {
   render() {
     const concerts = this.state.concerts
     return (
-      <div>
-        <main>
-          <h1>TUNE IN</h1>
-        </main>
-        <main>
-          {concerts.map(concert => {
-            return <Concert key={concert.id} concert={concert} />
-          })}
-        </main>
+      <div className="wrapper tutorial-page">
+        <div className="main main-raised main-translucent">
+          <div className="section">
+            <div className="container">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th className="text-center text-white">Date</th>
+                    <th className="text-white">Time</th>
+                    <th className="text-white">Artist</th>
+                    <th className="text-white">Venue</th>
+                    <th className="text-right text-white">Sale</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {concerts.map(concert => {
+                    return <Concert key={concert.id} concert={concert} />
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
+// {concerts.map(concert => {
+//   return <Concert key={concert.id} concert={concert} />
+// })}
 
 export default EventPage
