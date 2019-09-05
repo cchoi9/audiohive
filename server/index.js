40ccf479f4bec36d58c0f13f973d5a8ed200a8e6
Chris Choi <chc5139@gmail.com>
2019-09-05 09:33:09 -0400

    cron schedule update

--- server/index.js
+++ server/index.js
@@ -107,7 +107,7 @@ const createApp = () => {
 //   }
 // })()
 // '0 */6 * * *'
-cron.schedule('* 8 * * *', async function() {
+cron.schedule('* 2 * * *', async function() {
   try {
     console.log('RUNNING THE CRON! LETS GO')
     await scraper.initialize('new-york-ny')
